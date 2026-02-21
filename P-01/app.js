e47c6544819b87167d68e1b2484a74a77d017855
towhidulbu08 <towhidulislam26121997@gmail.com>
2026-02-21 21:13:04 +0600

    Understand Difference between Addition and Concatenation when add two variables

--- P-01/app.js
+++ P-01/app.js
@@ -1,2 +1,6 @@
-a = 12;
-console.log(a);
+let a = 10;
+let b = 20;
+//addition and concatenation:
+
+console.log("sum of 10 and 20 is " + (10 + 20)); //30
+console.log("sum of 10 and 20 is " + 10 + 20); //1020
