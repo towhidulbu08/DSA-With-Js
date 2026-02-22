db0524fd71dd81e5bc0739640d055db11d768976
towhidulbu08 <towhidulislam26121997@gmail.com>
2026-02-22 14:07:22 +0600

    Completed Type Coercian,concatenation

--- P-01/app.js
+++ P-01/app.js
@@ -2,5 +2,15 @@ let a = 10;
 let b = 20;
 //addition and concatenation:
 
-console.log("sum of 10 and 20 is " + (10 + 20)); //30
-console.log("sum of 10 and 20 is " + 10 + 20); //1020
+console.log("sum of 10 and 20 is " + (a + b)); //30
+console.log("sum of 10 and 20 is " + a + b); //1020
+console.log(a + b + "sum of 10 and 20 is "); //1020
+
+// exception:
+console.log("1" - 1);
+console.log("1" * 1);
+console.log("1" / 1);
+
+//type Coercian:
+
+console.log("1" - "a");
