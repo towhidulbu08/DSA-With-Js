c5c2ddf2206c73d8004aef934dab12a9ed886a67
towhidulbu08 <towhidulislam26121997@gmail.com>
2026-02-22 14:19:29 +0600

    Completed how to take user input using prompt() function and type casting

--- P-01/app.js
+++ P-01/app.js
@@ -1,16 +1,4 @@
-let a = 10;
-let b = 20;
-//addition and concatenation:
+let age = Number(prompt("What is your age"));
+console.log("age", age);
 
-console.log("sum of 10 and 20 is " + (a + b)); //30
-console.log("sum of 10 and 20 is " + a + b); //1020
-console.log(a + b + "sum of 10 and 20 is "); //1020
-
-// exception:
-console.log("1" - 1);
-console.log("1" * 1);
-console.log("1" / 1);
-
-//type Coercian:
-
-console.log("1" - "a");
+//type casting:
