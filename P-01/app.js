c24a637b514607435f18cf03f71cc64eecb9b266
towhidulbu08 <towhidulislam26121997@gmail.com>
2026-02-22 20:37:06 +0600

    Completed how to swap two variables value in different way

--- P-01/app.js
+++ P-01/app.js
@@ -1,4 +1,31 @@
-let age = Number(prompt("What is your age"));
-console.log("age", age);
+// Value Swaping , three methods:
 
-//type casting:
+//01:Using Extra Variable:
+
+let a = 10;
+let b = 20;
+let c;
+// c = a; // c=10, a=10
+// a = b; //a=20,b=20
+// b = c; //b=10
+
+// console.log("a", a);
+// console.log("b", b);
+
+//02:
+
+// a = a + b; //a=30
+// b = a - b; //b=10
+// a = a - b; //a=10
+
+//03:destructuring assignment:
+
+[a, b] = [b, a];
+
+// console.log("a", a);
+// console.log("b", b);
+
+let x = 7;
+let y = 2;
+console.log(x / y);
+console.log(x % y);
