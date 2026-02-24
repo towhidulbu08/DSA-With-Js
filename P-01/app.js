f226a2e628af1898d0f4b4a6215c1714531a8d07
towhidulbu08 <towhidulislam26121997@gmail.com>
2026-02-24 20:48:36 +0600

    Completed Math() Functions With Examples and Different usecases

--- P-01/app.js
+++ P-01/app.js
@@ -1,22 +1,54 @@
-// division and modulus and quotient and remainder:
+//unary operator:
 
-// let x = 7;
-// let y = 2;
-// console.log(x / y);
-// console.log(x % y);
+// let a = 10;
+// let b = a++; //postfix increment operator
+// console.log(a); //11
+// console.log(b); //10
 
-// let a1 = 865689;
-// console.log(a1 / 10);
-// console.log(a1 % 10);
+// let c = 20;
+// let d = ++c; //prefix increment operator
+// console.log(c);
+// console.log(d); //21
 
-//?How to remove the last digit of a number?
+//Questions:
 
-let a2 = 865689;
-console.log(Math.floor(a2 / 10));
-// console.log(Math.floor(a2 / 100));
+// let i = 11;
+// i = i++ + ++i; //24
+// console.log("i", i);
 
-//?How to remove the last two digits of a number?
+// let a = 11,
+//   b = 22;
 
-let a3 = 865689;
-console.log(Math.floor(a3 / 100));
-console.log(Math.floor(a3 / 100));
+// let c = a + b + a++ + b++ + ++a + ++b; // 11+22+11+22+13+24
+
+// console.log("a=" + a);
+// console.log("b=" + b);
+// console.log("c=" + c);
+
+// let a = true;
+
+// a++;
+
+// console.log("a", a);
+
+//let a=11++;
+//console.log("a", a); // give error
+
+//Math Functions:
+
+console.log(Math.round(2.4)); //3
+
+console.log(Math.ceil(10.01)); //11
+
+console.log(Math.floor(10.9)); //10
+
+console.log(Math.trunc(10.8998));
+
+console.log(Math.pow(3, 3));
+
+console.log(Math.sqrt(4));
+console.log(Math.cbrt(8));
+
+console.log(Math.trunc(Math.random() * 9000) + 1000);
+
+console.log((20.3293).toFixed(1));
