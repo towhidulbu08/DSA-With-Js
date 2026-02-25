476d8f1cb3a74a81a6eab9fb8f35f1c6f4438af2
towhidulbu08 <towhidulislam26121997@gmail.com>
2026-02-25 20:27:37 +0600

    Start Part-02

--- P-01/app.js
+++ P-01/app.js
@@ -1,54 +1,11 @@
-//unary operator:
+//Problem: find area of a triangle using Heron's formula
+//Solution: use the formula A = sqrt(s(s-a)(s-b)(s-c)) where s = (a+b+c)/2
 
-// let a = 10;
-// let b = a++; //postfix increment operator
-// console.log(a); //11
-// console.log(b); //10
+let a = 5;
+let b = 6;
+let c = 7;
 
-// let c = 20;
-// let d = ++c; //prefix increment operator
-// console.log(c);
-// console.log(d); //21
+let s = (a + b + c) / 2;
 
-//Questions:
-
-// let i = 11;
-// i = i++ + ++i; //24
-// console.log("i", i);
-
-// let a = 11,
-//   b = 22;
-
-// let c = a + b + a++ + b++ + ++a + ++b; // 11+22+11+22+13+24
-
-// console.log("a=" + a);
-// console.log("b=" + b);
-// console.log("c=" + c);
-
-// let a = true;
-
-// a++;
-
-// console.log("a", a);
-
-//let a=11++;
-//console.log("a", a); // give error
-
-//Math Functions:
-
-console.log(Math.round(2.4)); //3
-
-console.log(Math.ceil(10.01)); //11
-
-console.log(Math.floor(10.9)); //10
-
-console.log(Math.trunc(10.8998));
-
-console.log(Math.pow(3, 3));
-
-console.log(Math.sqrt(4));
-console.log(Math.cbrt(8));
-
-console.log(Math.trunc(Math.random() * 9000) + 1000);
-
-console.log((20.3293).toFixed(1));
+let area = Math.sqrt(s * (s - a) * (s - b) * (s - c)).toFixed(2);
+console.log("area of triangle", area);
---
+++ P-02/app.js
@@ -0,0 +1,5 @@
+if (112 > 3) {
+  console.log("112 is greater than 3");
+} else {
+  console.log("112 is not greater than 3");
+}
