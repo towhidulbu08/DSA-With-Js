b7e4e719f83d96661ba9d1ee1952c14b24698d65
towhidulbu08 <towhidulislam26121997@gmail.com>
2026-03-03 14:16:20 +0600

    Solved Dicount Problem Using Conditionals

--- P-02/app.js
+++ P-02/app.js
@@ -1,5 +1,23 @@
-if (112 > 3) {
-  console.log("112 is greater than 3");
+let amount = Number(prompt("Enter the amount: "));
+console.log("amount", amount);
+let discount;
+let priceAfterDiscount;
+let payblePrice;
+
+function calculatePrice(x) {
+  discount = x;
+  priceAfterDiscount = (discount * amount) / 100;
+  payblePrice = amount - priceAfterDiscount;
+  console.log("payable price", Math.floor(payblePrice));
+}
+if (amount > 0 && amount <= 5000) {
+  calculatePrice(0);
+} else if (amount >= 5001 && amount <= 7000) {
+  calculatePrice(5);
+} else if (amount >= 7001 && amount <= 9000) {
+  calculatePrice(10);
+} else if (amount > 9000) {
+  calculatePrice(20);
 } else {
-  console.log("112 is not greater than 3");
+  console.log("Invalid Input");
 }
